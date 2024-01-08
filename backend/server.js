bfea258265b3010f903141e451f9e0e68354912f
hazem_smawy <hazemsmawy@gmail.com>
2024-01-08 15:47:53 +0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const dotenv = require("dotenv").config()
+const colors = require("colors")
+const {errorHander} = require('./middleware/errorMiddleWare')
+
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+
+
+connectDB()
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+app.use('/api/goals',require('./routes/goalRoutes'))
+app.use(errorHander)
+app.listen(port,()=> console.log(`Server stared on port: ${port} `))
